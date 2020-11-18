ade657024c5331b91a6d47121bc6d25ca538d50a
cliod <qi2415@qq.com>
2020-11-18 14:53:32 +0800

    test: 测试

--- jd_test.go
+++ jd_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestServiceImpl(t *testing.T) {
@@ -17,16 +18,12 @@ func TestServiceImpl(t *testing.T) {
 
 	// goods api
 	g := j.GetGoodsService()
-
-	//res, err := g.GoodsJingfenQuery(&GoodsJingfenQueryRequest{
-	//	EliteId: 33,
-	//})
-	//t.Log(res, err)
-	//if err != nil {
-	//	return
-	//}
-	//t.Log(string(res.GetResult()), err)
-
+	res, err := g.GoodsJingfenQuery(&GoodsJingfenQueryRequest{
+		EliteId: 33,
+	})
+	t.Log(res, err)
+	res0, err := g.GoodsQuery(&GoodsQueryRequest{})
+	t.Log(res0, err)
 	res1, err := g.GoodsPromotiongoodsinfoQuery("63682765730")
 	t.Log(res1, err)
 
@@ -35,12 +32,24 @@ func TestServiceImpl(t *testing.T) {
 		Grade:    0,
 	})
 	t.Log(res2, err)
-
 	res3, err := g.GoodsGigfieldQuery(&GoodsGigfieldRequest{
 		SkuIds: []uint64{63682765730},
 	})
 	t.Log(res3, err)
 
+	// coupon api
+	cc := j.GetCouponService()
+	res4, err := cc.CouponQueryByList("https://coupon.m.jd.com/coupons/show.action?linkKey=AAROH_xIpeffAs_-naABEFoeRYiv3hJ69zyA-kQJMnHSr14qXhdrY5zeYZRSj6boZw2cJf-LDnD4b3HpA9b_Y_bjbeXiHA")
+	t.Log(res4, err)
+
+	o := j.GetOrderService()
+	res5, err := o.OrderQuery(&OrderQueryRequest{
+		PageNo:   1,
+		PageSize: 30,
+		TypeNum:  1,
+		Time:     time.Now().Format("yyyyMMddHHmm"),
+	})
+	t.Log(res5, err)
 }
 
 func TestParameter_AttachSign(t *testing.T) {
